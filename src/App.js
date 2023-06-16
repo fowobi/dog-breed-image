c61a130506c436c88f144442ef7044ceb8abfdc2
fowobi <109865505+fowobi@users.noreply.github.com>
2023-06-16 22:01:13 +0100

    updated dog-breed-image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [imgSrc, setImgSrc] = useState(undefined);
+  const [breed, setBreed] = useState("hound");
+  const [imgNumber, setImgNumber] = useState(0);
+
+  const getDogImage = () => {
+    fetch(`https://dog.ceo/api/breed/${breed}/images`)
+      .then((res) => res.json())
+      .then((data) => setImgSrc(data.message[imgNumber]));
+  };
+
+  const onImgNumberChanged = (event) => setImgNumber(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Show a dog image</p>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          getDogImage();
+        }}
+      >
+        <input className="breed-name"
+          id="breed-name"
+          type="text"
+          value={breed}
+          onChange={(event) => setBreed(event.target.value)}
+        />
+        <input type="number" value={imgNumber} onChange={onImgNumberChanged} />
+        <button type="submit">Show me the image</button>
+      </form>
+      <br />
+
+      <div className="soww">
+        {imgSrc === undefined ? (
+          "No image to show"
+        ) : (
+          <img src={imgSrc} alt="Dog" />
+        )}
+      </div>
     </div>
   );
 }
